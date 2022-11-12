6c77d639e4d8c6394ced6cc4b4a96de9d1246b3d
Olayinka Bidemi Ayeni <65237847+YinkTech@users.noreply.github.com>
2022-11-12 14:55:53 +0100

    refactor redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { Provider} from 'react-redux'
+import store from './store/store'
+import { Provider } from 'react-redux'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    //<Provider store={store}>
-        <App />
-    //</Provider>
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
 );
