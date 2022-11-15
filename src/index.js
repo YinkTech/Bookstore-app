08dc411c7088d869228a533b4cbea0fa2f89fb2b
Olayinka Bidemi Ayeni <65237847+YinkTech@users.noreply.github.com>
2022-11-15 18:16:14 +0100

    add css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './components/App';
 import store from './store/store'
 import { Provider } from 'react-redux'
+import './index.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
